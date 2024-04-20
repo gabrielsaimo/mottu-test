9dd66b7d36f660403f47a89946b60bc79e303506
gabrielsaimo <gabrielsaimo68@gmail.com>
2024-04-19 23:38:37 -0300

    Adicionar injeção de dependência para o serviço de análise de dados no app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { inject } from '@vercel/analytics';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: 'production' });
+      },
+    },
   ],
 };
